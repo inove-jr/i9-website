841f4594c8b4bc784782553cae861b4ff86c2433
mateusjrcavalcanti <mateusjrcavalcanti@outlook.com>
2024-07-16 21:39:18 -0300

    refactor: change tailwindcss colors name

--- src/app/(homepage)/_components/Portfolio.tsx
+++ src/app/(homepage)/_components/Portfolio.tsx
@@ -1,5 +1,5 @@
 export const Portfolio = () => (
-  <section className='flex h-72 w-full items-center justify-center bg-i9-blue-900 text-i9-white-100'>
-    <h2 className="decorated-border">PORTFÓLIO</h2>
+  <section className='text-white-100 flex h-72 w-full items-center justify-center bg-blue-900'>
+    <h2 className='decorated-border'>PORTFÓLIO</h2>
   </section>
 );
